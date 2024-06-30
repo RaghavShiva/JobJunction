9bf274494b5f11e6ff79ec103bac35cde8cd3afd
RaghavShiva <shiva26122004@gmail.com>
2024-06-30 17:13:27 +0530

    CRUD ops done on jobs

--- controllers/jobcont.js
+++ controllers/jobcont.js
@@ -18,3 +18,44 @@ export const getJobs = async (req, res, next) => {
         jobs
     })
 }
+
+//update fn
+export const updateJobs = async (req, res, next) => {
+    const { id } = req.params
+    const { company, position } = req.body
+    if (!company || !position)
+        next('please provide all fields')
+    const job = await jobsModel.findOne({ _id: id })
+    if (!job)
+        next(`NO jobs found with this id ${id}`)
+    if (req.user.userId !== job.createdBy.toString()) {
+        next('you are not authorized to update this job')
+        return
+    }
+
+
+    const updateJob = await jobsModel.findOneAndUpdate({ _id: id }, req.body, {
+        new: true,
+        runValidators: true
+    })
+    res.status(200).json({
+        updateJob
+    })
+}
+
+//delete fn
+export const deleteJobCont = async (req, res, next) => {
+    const { id } = req.params
+    const job = await jobsModel.findOne({ _id: id })
+    if (!job)
+        next(`NO job found with this id ${id}`)
+    if (req.user.userId !== job.createdBy.toString()) {
+        next('you are not authorized to delete this job')
+        return
+    }
+    await job.deleteOne()
+    res.status(200).json({
+        message: "Success, Job Deleted!"
+    })
+
+}
--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import userAuth from '../middlewares/authmiddleware.js'
-import { createJobCont, getJobs } from '../controllers/jobcont.js'
+import { createJobCont, deleteJobCont, getJobs, updateJobs } from '../controllers/jobcont.js'
 
 const router = express.Router()
 
 router.post('/create-job', userAuth, createJobCont)
+
 router.get('/get-job', userAuth, getJobs)
+
+router.patch('/update-job/:id', userAuth, updateJobs)
+
+router.delete('/delete-job/:id', userAuth, deleteJobCont)
+
 export default router
