6c8945cf307e36650a0c2595f03a5ddab071212d
RaghavShiva <shiva26122004@gmail.com>
2024-06-30 22:07:03 +0530

    stats filtering done

--- controllers/jobcont.js
+++ controllers/jobcont.js
@@ -1,5 +1,6 @@
 import jobsModel from "../models/jobsModel.js"
-
+import mongoose from 'mongoose'
+import moment from 'moment'
 //create fn
 export const createJobCont = async (req, res, next) => {
     const { company, position } = req.body
@@ -58,4 +59,58 @@ export const deleteJobCont = async (req, res, next) => {
         message: "Success, Job Deleted!"
     })
 
+}
+
+export const statsJobCont = async (req, res) => {
+    const stats = await jobsModel.aggregate([
+        // seach by jobs
+        {
+            $match: {
+                createdBy: new mongoose.Types.ObjectId(req.user.userId)
+            }
+        },
+        {
+            $group: {
+                _id: '$status',
+                count: { $sum: 1 }
+            }
+        }
+    ])
+    // default stats
+    const defstats = {
+        pending: stats.pending || 0,
+        reject: stats.reject || 0,
+        interview: stats.interview || 0
+    }
+
+    //monthly/yearly
+    let monthlyApp = await jobsModel.aggregate([
+        {
+            $match: {
+                createdBy: new mongoose.Types.ObjectId(req.user.userId)
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    year: { $year: '$createdAt' },
+                    month: { $month: '$createdAt' }
+                },
+                count: {
+                    $sum: 1
+                }
+            }
+        }
+    ])
+    // formatting using moment library
+    monthlyApp = monthlyApp.map(item => {
+        const { _id: { year, month }, count } = item
+        const date = moment().month(month - 1).year(year).format('MMM Y')
+        return { date, count }
+    }).reverse()
+    res.status(200).json({
+        totalJob: stats.length,
+        defstats,
+        monthlyApp
+    })
 }
--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import userAuth from '../middlewares/authmiddleware.js'
-import { createJobCont, deleteJobCont, getJobs, updateJobs } from '../controllers/jobcont.js'
+import { createJobCont, deleteJobCont, getJobs, statsJobCont, updateJobs } from '../controllers/jobcont.js'
 
 const router = express.Router()
 
@@ -11,5 +11,7 @@ router.get('/get-job', userAuth, getJobs)
 router.patch('/update-job/:id', userAuth, updateJobs)
 
 router.delete('/delete-job/:id', userAuth, deleteJobCont)
+
+router.get('/job-stats', userAuth, statsJobCont)
 
 export default router
