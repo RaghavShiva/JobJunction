dee62e609fee1a321c91367d50e7a2a1b93e3e6b
RaghavShiva <shiva26122004@gmail.com>
2024-07-04 22:05:04 +0530

    dashboard done

--- client/src/pages/DashBoard.js
+++ client/src/pages/DashBoard.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Layout from '../components/layout/Layout'
 
 const DashBoard = () => {
     return (
-        <div>
+        <Layout>
             <h1>DashBoard
             </h1>
-        </div>
+        </Layout>
     )
 }
 
